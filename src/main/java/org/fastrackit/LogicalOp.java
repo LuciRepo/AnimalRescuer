94138ff72c727bb0c5910b2e63edc90c22c614b0
Luci <ignalucian@yahoo.com>
2019-09-09 20:57:39 +0300

    Update_09.09.2019_2

---
+++ src/main/java/org/fastrackit/LogicalOp.java
@@ -0,0 +1,123 @@
+package org.fastrackit;
+
+public class LogicalOp {
+    public static void main(String[] args) {
+       // LogicalOp logicalOp = new LogicalOp();
+    }
+
+    public LogicalOp() {
+        //count100(100);
+        // countMinus100(-320);
+        //countBetweenAB(1,14);
+        //countBetweenABTwoWay(23,89);
+        //count1To100();
+        //countOdd1To100();
+        //sumTo100(99);
+       // averageTo100(29);
+        asterix(7);
+    }
+
+    /*public void count100(int numberGot) {
+        if (numberGot <= 100) {
+            for (int i = numberGot; i <= 100; i++) {
+                System.out.println(i);
+            }
+        }else {
+            for(int i=numberGot;i>=100;i--){
+                System.out.println(i);
+            }
+        }
+    }*/
+
+    /*public void countMinus100(int numberGot) {
+
+        if (numberGot <= -100) {
+            for (int i = numberGot; i <=-100; i++) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = numberGot; i >=-100; i--) {
+                System.out.println(i);
+            }
+        }
+    }*/
+
+    /*public void countBetweenAB(int numberA, int numberB) {
+        if (numberA <= numberB) {
+            for (int i = numberA; i <=numberB; i++) {
+                System.out.println(i);
+            }
+        } else {
+            System.out.println("Nu ati introdus corect cele 2 numere");
+            for (int i = numberB; i<=numberA; i++) {
+                System.out.println(i);
+            }
+        }
+    }*/
+
+   /* public void countBetweenABTwoWay(int numberA, int numberB) {
+        if (numberA <= numberB) {
+            for (int i = numberA; i <=numberB; i++) {
+                System.out.println(i);
+            }
+        } else {
+            //System.out.println("Nu ati introdus corect cele 2 numere");
+            for (int i = numberB; i<=numberA; i++) {
+                System.out.println(i);
+            }
+        }
+    }*/
+
+    /* public void count1To100() {
+         System.out.println("Numerele de la 1 la 100 sunt ");
+         for (int i = 1; i <= 100; i++) {
+             System.out.println(i);
+         }
+     }*/
+    /*public void countOdd1To100() {
+        System.out.println("Numerele impare de la 1 la 100 sunt ");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            } else {
+            }
+        }
+    }*/
+   /* public void sumTo100(int numberGot) {
+        int sum = 0;
+        if (numberGot <= 100) {
+            for (int i = numberGot; i <= 100; i++) {
+                sum = sum + i;
+            }
+            System.out.println("Suma ceruta este : " + sum);
+        } else {
+            System.out.println("Introduceti un numar mai mic ca 100");
+            return;
+        }
+    }*/
+
+    /*public void averageTo100(int numberGot) {
+        int sum = 0;
+        double count = 0;
+        if (numberGot <= 100) {
+            for (int i = numberGot; i <= 100; i++) {
+                sum = sum + i;
+                count++;
+            }
+            System.out.println("Media ceruta este : " + sum / count);
+        } else {
+            System.out.println("Introduceti un numar mai mic ca 100");
+            return;
+        }
+    }*/
+
+    public void asterix(int number){
+        for(int i=1;i<=7;i++){
+            for(int j=i;j<7;j++){
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+    }
+}
+
